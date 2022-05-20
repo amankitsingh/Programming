552c7fac3920290fa4de6f9e41bd3dffc5292265
Ankit Singh <1999ankits@gmail.com>
2022-05-20 23:13:03 +0530

    Update 104-maximum-depth-of-binary-tree.py
    Add 2 solution 
    recursive and iterative

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -1,3 +1,4 @@
+# Solution 1: Recursive
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,3 +12,22 @@ class Solution:
                 return level
             return max(dfh(root.left,level+1),dfh(root.right,level+1))
         return dfh(root)
+    
+# Solution 2: Iterative
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        que = deque([root])
+        level = 0
+        while que:
+            level+=1
+            for _ in range(len(que)):
+                temp = que.popleft()
+                if temp.left:
+                    que.append(temp.left)
+                if temp.right:
+                    que.append(temp.right)
+                    
+         return level
