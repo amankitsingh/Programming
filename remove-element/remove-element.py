961189ab4d4a360a123680b693324220eb924e58
Ankit Singh <1999ankits@gmail.com>
2023-06-11 17:21:53 +0530

    Time: 54 ms, Memory: 16.1 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -2,7 +2,7 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
         for x in nums:
-            if x!=val:
+            if x != val:
                 nums[i] = x
                 i+=1
         return i
