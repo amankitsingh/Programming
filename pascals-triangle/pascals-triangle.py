ef6ba8b4a8e9aa12b04fb42fd53434651e576b9f
Ankit Singh <1999ankits@gmail.com>
2023-06-12 16:28:49 +0530

    Time: 55 ms, Memory: 16.1 MB - LeetHub

--- pascals-triangle/pascals-triangle.py
+++ pascals-triangle/pascals-triangle.py
@@ -3,7 +3,7 @@ class Solution:
         result = [[1]*x for x in range(1,numRows+1)]
             
         for i in range(2,numRows):
-            for j in range(1,len(result[i])-1):
+            for j in range(1,i):
                 if j!=0 and j!=len(result[i])-1:
                     result[i][j] = result[i-1][j-1]+result[i-1][j]
         return result
