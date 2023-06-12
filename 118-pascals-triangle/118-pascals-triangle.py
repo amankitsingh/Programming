2e08b795dbf0c3a698c3a37712c017b685597ff2
Ankit Singh <1999ankits@gmail.com>
2023-06-12 16:30:31 +0530

    Update 118-pascals-triangle.py
    optimize code

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,3 +1,4 @@
+#Answer1
 class Solution:
     def get_array(self, position, array)-> List[List[int]]:
         result = []
@@ -17,3 +18,14 @@ class Solution:
         for _ in range(1,numRows):
             result.append(self.get_array(_ + 1, result[_ - 1]))
         return result
+    
+#Answer2
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = [[1]*x for x in range(1,numRows+1)]
+            
+        for i in range(2,numRows):
+            for j in range(1,i):
+                result[i][j] = result[i-1][j-1]+result[i-1][j]
+        return result
+        
