11b9093bd9fcc9a2fd6c4278213c5cacff6f5c34
Ankit Singh <1999ankits@gmail.com>
2023-06-12 16:26:59 +0530

    Time: 47 ms, Memory: 16.2 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for x in range(1,numRows+1):
+            result.append([1]*x)
+            
+        for i in range(2,numRows):
+            for j in range(1,len(result[i])-1):
+                if j!=0 and j!=len(result[i])-1:
+                    result[i][j] = result[i-1][j-1]+result[i-1][j]
+        return result
+        
