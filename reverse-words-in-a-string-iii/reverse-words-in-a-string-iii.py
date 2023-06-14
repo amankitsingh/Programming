b1c011ac9b411938ce6e4a061d633895a53d96b3
Ankit Singh <1999ankits@gmail.com>
2023-06-15 00:12:19 +0530

    Update reverse-words-in-a-string-iii.py
    answer 2 updated

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -2,3 +2,11 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join((i[::-1] for i in s.split()))
         
+#Answer 2
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        result = s.split(" ")
+        for i in range(len(result)):
+            result[i] = "".join(reversed(result[i]))
+        return " ".join(result)
+            
