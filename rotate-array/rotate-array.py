4337b0e5f78ad1cc3f29d330a0615590465274ff
Ankit Singh <1999ankits@gmail.com>
2023-06-14 23:59:27 +0530

    Update rotate-array.py
    python power

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -14,3 +14,17 @@ class Solution:
         self.reverse(nums, len(nums)-k, len(nums)-1)
         self.reverse(nums, 0 , len(nums)-1)
         
+#Python power
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        # calculate the number of steps we actually need to take
+        k = k % len(nums)
+
+        # reverse the entire array
+        nums.reverse()
+
+        # reverse the first k elements
+        nums[:k] = reversed(nums[:k])
+
+        # reverse the remaining elements
+        nums[k:] = reversed(nums[k:])
