7e443e11551231cece1cb9bb015b462871bdc3ad
Ankit Singh <1999ankits@gmail.com>
2023-06-14 18:41:16 +0530

    Time: 320 ms, Memory: 19.4 MB - LeetHub

--- array-partition-i/array-partition-i.py
+++ array-partition-i/array-partition-i.py
@@ -3,10 +3,12 @@ class Solution:
         i = 0
         result = []
         nums = sorted(nums)
+        
         while i < len(nums):
             result.append([nums[i],nums[i+1]])
             i+=2
         output = 0
+        
         for x in result:
             output+=min(x[0],x[1])
         return output
