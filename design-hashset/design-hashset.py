2a309b134c58ab6ea471bd854f76602576700656
Ankit Singh <1999ankits@gmail.com>
2023-06-15 23:41:18 +0530

    Update design-hashset.py
    small answer

--- design-hashset/design-hashset.py
+++ design-hashset/design-hashset.py
@@ -37,3 +37,22 @@ class MyHashSet:
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+
+#Answer 2
+class MyHashSet:
+
+    def __init__(self):
+        self.d={}
+
+    def add(self, key: int) -> None:
+        if key not in self.d:
+            self.d[key]=key
+        else:
+            self.d[key]+=key
+
+    def remove(self, key: int) -> None:
+        if key in self.d:
+            self.d.pop(key)
+
+    def contains(self, key: int) -> bool:
+            return key in self.d
