eb0ec53302f031dcdd3f4c39c3b8224ea982dd61
Ankit Singh <1999ankits@gmail.com>
2023-06-16 18:48:47 +0530

    Update intersection-of-two-arrays-ii.py
    2 answer

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -8,3 +8,13 @@ class Solution:
                 result.append(x)
                 c[x] -=1
         return result
+    
+# Answer 2
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        for x in range(len(nums2)):
+            if nums2[x] in nums1:
+                result.append(nums2[x])
+                nums1[nums1.index(nums2[x])] = -1
+        return result
