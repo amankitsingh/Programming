2742b7bc66ca7a3bcfcf9c85d0f338f4ad5bbbaf
Ankit Singh <1999ankits@gmail.com>
2023-06-16 16:04:53 +0530

    Time: 84 ms, Memory: 17.7 MB - LeetHub

---
+++ 0two-sum/0two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in hashmap and hashmap[comp]!=i:
+                return [i,hashmap[comp]]
