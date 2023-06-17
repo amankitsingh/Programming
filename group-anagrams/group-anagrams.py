124bb977ca2bab7abf95c72e5f8e3f5b4dbfb55d
Ankit Singh <1999ankits@gmail.com>
2023-06-17 22:16:49 +0530

    Time: 121 ms, Memory: 19.6 MB - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+        
+        for x in strs:
+            temp = "".join(sorted(x))
+            if temp in hashmap:
+                hashmap[temp].append(x)
+            else:
+                hashmap[temp] = [x]
+        
+        return list(hashmap.values())
