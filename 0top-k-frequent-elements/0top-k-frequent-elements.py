663af26b991a95760d09129ea24afb111e85e19d
Ankit Singh <1999ankits@gmail.com>
2023-06-18 20:52:03 +0530

    Update 0top-k-frequent-elements.py
    using heapq

--- 0top-k-frequent-elements/0top-k-frequent-elements.py
+++ 0top-k-frequent-elements/0top-k-frequent-elements.py
@@ -11,3 +11,19 @@ class Solution:
             else:
                 break
         return result
+
+#Answer 2
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        # O(1) time 
+        if k == len(nums):
+            return nums
+        
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)   
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key=count.get) 
