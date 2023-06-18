accfd62f14622b4106c3431396328300d51f6450
Ankit Singh <1999ankits@gmail.com>
2023-06-18 17:52:39 +0530

    Time: 56 ms, Memory: 16.3 MB - LeetHub

--- jewels-and-stones/jewels-and-stones.py
+++ jewels-and-stones/jewels-and-stones.py
@@ -1,6 +1,5 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewels = list(jewels)
         result = 0
         for i in list(stones):
             if i in jewels:
