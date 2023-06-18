18ed8ba174f1ce3997c56ad88cd480b5f24dc229
Ankit Singh <1999ankits@gmail.com>
2023-06-18 22:31:45 +0530

    Time: 430 ms, Memory: 63.7 MB - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,26 +1,25 @@
-class RandomizedSet:
+import random
+
+class RandomizedSet(object):
 
     def __init__(self):
-        self.hashset = set()
-
-    def insert(self, val: int) -> bool:
-        if val not in self.hashset:
-            self.hashset.add(val)
+        self.nums, self.pos = [], {}
+        
+    def insert(self, val):
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
             return True
         return False
+        
 
-    def remove(self, val: int) -> bool:
-        if val in self.hashset:
-            self.hashset.remove(val)
+    def remove(self, val):
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.nums[idx], self.pos[last] = last, idx
+            self.nums.pop(); self.pos.pop(val, 0)
             return True
         return False
-
-    def getRandom(self) -> int:
-        return random.sample(self.hashset,1)[0]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+            
+    def getRandom(self):
+        return self.nums[random.randint(0, len(self.nums) - 1)]
