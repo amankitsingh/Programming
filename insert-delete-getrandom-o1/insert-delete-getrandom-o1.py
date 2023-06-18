c3770fcf544864751464624cb99ae399c2a7b5fb
Ankit Singh <1999ankits@gmail.com>
2023-06-18 22:27:57 +0530

    Time: 902 ms, Memory: 63.9 MB - LeetHub

---
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -0,0 +1,26 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.hashset = set()
+
+    def insert(self, val: int) -> bool:
+        if val not in self.hashset:
+            self.hashset.add(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.hashset:
+            self.hashset.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.sample(self.hashset,1)[0]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
