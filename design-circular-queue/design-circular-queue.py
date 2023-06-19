bb0e69b388907de5407754b2af01c3e7c3e3c826
Ankit Singh <1999ankits@gmail.com>
2023-06-19 19:25:54 +0530

    Update design-circular-queue.py
    answer 2

--- design-circular-queue/design-circular-queue.py
+++ design-circular-queue/design-circular-queue.py
@@ -43,7 +43,54 @@ class MyCircularQueue:
             return True
         return False
 
+#Answer 2
+class MyCircularQueue:
 
+    def __init__(self, k: int):
+        self.capacity = k
+        self.size = 0
+        self.head = 0
+        self.data = [0] * k
+        
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        end = (self.head + self.size) % self.capacity
+        self.data[end] = value
+        self.size += 1
+        return True
+        
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.head = (self.head + 1) % self.capacity
+        self.size -= 1
+        return True
+
+        
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.data[self.head]
+        
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        end = (self.head + self.size - 1) % self.capacity
+        return self.data[end]
+        
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+        
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
+        
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
 # param_1 = obj.enQueue(value)
