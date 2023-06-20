11c557242a53479e31b73fd6690d02a56bb78e3c
Ankit Singh <1999ankits@gmail.com>
2023-06-20 16:50:24 +0530

    Update number-of-islands.py

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -17,12 +17,17 @@ class Solution:
                     result += search_neighbour(grid, i,j)
         return result
 
+# Map function works horizontally, whereas the zip function work vertically.
+
 #Answer 2
-def numIslands(self, grid):
-    def sink(i, j):
-        if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
-            grid[i][j] = '0'
-            map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1)) # Map function works horizontally, where as the zip function work vertically.
-            return 1
-        return 0
-    return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
+class Solution:
+    def numIslands(self, grid):
+        def sink(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '0'
+                d = [0,1,0,-1,0]
+                for k in range(4):
+                    sink((i+d[k]), (j+d[k+1]))
+                return 1
+            return 0
+        return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
