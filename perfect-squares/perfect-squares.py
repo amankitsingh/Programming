8d5c874d795bc21c7b1b80e1f9bd75a705797a1d
Ankit Singh <1999ankits@gmail.com>
2023-06-20 23:40:41 +0530

    Update perfect-squares.py

--- perfect-squares/perfect-squares.py
+++ perfect-squares/perfect-squares.py
@@ -1,5 +1,19 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        dp = [n] * (n+1)
+        dp[0] = 0
+        for target in range(1, n+1):
+            for squ in range(1, target+1):
+                square = squ*squ
+                if target - square < 0:
+                    break
+                dp[target] = min(dp[target], 1 + dp[target-square])
+           
+        return dp[n]
+
+#Answer 2 - Faster
+class Solution:
+    def numSquares(self, n: int) -> int:
         squares = [i**2 for i in range(1, int(math.sqrt(n) + 1))]
 
         level = 0
@@ -13,10 +27,12 @@ class Solution:
             for remainder in que:
                 for square in squares:
                     if remainder == square:
+                        print(remainder,square)
                         return level
                     if square > remainder:
                         break
                     
                     nextQue.add(remainder - square)
             que = nextQue
+            print("outside", que)
         return level
