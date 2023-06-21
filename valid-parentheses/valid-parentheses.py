7a0133d4b8cedbbb94c2933ef1e35df4af65629b
Ankit Singh <1999ankits@gmail.com>
2023-06-21 14:57:10 +0530

    Time: 56 ms, Memory: 16.5 MB - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -3,12 +3,10 @@ class Solution:
         stack = []
         valid_paran = {"(":")", "[":"]","{":"}"}
         for i in s:
-            if i in valid_paran.keys():
+            if i in valid_paran:
                 stack.append(i)
             elif i in valid_paran.values():
-                if not stack:
-                    return False
-                if (stack and i != valid_paran[stack.pop()]):
+                if not stack or (stack and i != valid_paran[stack.pop()]):
                     return False
         
         return True if not stack else False
