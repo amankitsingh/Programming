bb8d92620d35319611d755c954192463ae30dbdd
Ankit Singh <1999ankits@gmail.com>
2023-06-21 14:57:33 +0530

    Time: 49 ms, Memory: 16.3 MB - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -6,7 +6,7 @@ class Solution:
             if i in valid_paran:
                 stack.append(i)
             elif i in valid_paran.values():
-                if not stack or (stack and i != valid_paran[stack.pop()]):
+                if not stack or (i != valid_paran[stack.pop()]):
                     return False
         
         return True if not stack else False
