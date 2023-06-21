c6aa20178db04808287af6e33ffe50321f409007
Ankit Singh <1999ankits@gmail.com>
2023-06-21 14:59:13 +0530

    Update valid-parentheses.py
    2answer

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -10,3 +10,23 @@ class Solution:
                     return False
         
         return True if not stack else False
+
+#Answer 2
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pMap = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+            }
+        stack = []
+
+        for c in s:
+            if c not in pMap:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != pMap[c]:
+                return False
+            stack.pop()
+
+        return not stack
