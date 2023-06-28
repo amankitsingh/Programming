a557781d770375771898a4c72ec0daaff49c5d8c
Ankit Singh <1999ankits@gmail.com>
2023-06-28 17:01:23 +0530

    Time: 44 ms, Memory: 16.4 MB - LeetHub

--- decode-string/decode-string.py
+++ decode-string/decode-string.py
@@ -1,22 +1,6 @@
 from collections import deque
 class Solution:
-    def decodeString(self, s: str) -> str:
-        decoding_stack = deque()
-        temp = deque()
-        for i in s:
-            if i == "]":
-                multiple=""
-                while len(decoding_stack[-1]) > 1 or not (48<= ord(decoding_stack[-1])<=57):
-                    temp.appendleft(decoding_stack.pop())
-                temp.popleft()
-                while decoding_stack and not len(decoding_stack[-1]) > 1 and (48<= ord(decoding_stack[-1])<=57):
-                    multiple += decoding_stack.pop()
-                multiple = multiple[::-1]
-                decoding_stack.append(''.join(temp)*int(multiple))
-                temp = deque()
-                multiple = ""
-            else:
-                decoding_stack.append(i)
-        return ''.join(decoding_stack)
-
-
+    def decodeString(self, s):
+        while '[' in s:
+            s = re.sub(r'(\d+)\[([a-z]*)\]', lambda m: int(m.group(1)) * m.group(2), s)
+        return s
