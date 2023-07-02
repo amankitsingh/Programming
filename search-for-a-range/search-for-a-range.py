83377e9784e712fda3c43102b9278a33007280d7
Ankit Singh <1999ankits@gmail.com>
2023-07-02 20:52:57 +0530

    Update search-for-a-range.py

--- search-for-a-range/search-for-a-range.py
+++ search-for-a-range/search-for-a-range.py
@@ -1,3 +1,20 @@
+# Answer 1 - using 2 pointers - O(n)
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if nums and nums[0]<= target and nums[len(nums)-1] >= target:
+            if target-nums[0] >= 0:
+                first_pointer = 0
+                last_pointer = len(nums)-1
+                while (first_pointer < last_pointer) and (nums[first_pointer] != target or nums[last_pointer] != target):
+                    if nums[first_pointer] < target:
+                        first_pointer+=1
+                    if nums[last_pointer] > target:
+                        last_pointer-=1
+                return [first_pointer,last_pointer] if nums[first_pointer] == target else [-1,-1]
+        
+        return [-1,-1]
+
+# Answer 2 - using binary search - O(log n + first + last) = O(log n)
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         first,last,first_target_index = 0, len(nums)-1, -1
