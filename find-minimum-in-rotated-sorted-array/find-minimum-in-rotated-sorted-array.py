784095a2ee9b6d4734b1d3675fc1de7654302ece
Ankit Singh <1999ankits@gmail.com>
2023-07-02 18:10:18 +0530

    Update find-minimum-in-rotated-sorted-array.py

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,3 +1,4 @@
+# Answer 1 - Binary search template 2
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
@@ -10,5 +11,20 @@ class Solution:
             else:
                 right = mid
         return nums[left]
-                
+
+# Answer 2 - Binary search with min value template 1
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        L, R = 0, len(nums) - 1
+        res = nums[0]
+
+        while L < R:
+            mid = (L + R) // 2
+            res = min(res, nums[mid])
+
+            if nums[mid] > nums[R]:
+                L = mid + 1
+            else:
+                R = mid - 1
+        return min(res, nums[L])
             
