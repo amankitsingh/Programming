fd0bcc7bdfbb46d0e2bf53be33fd3e5b3c9bc960
Ankit Singh <1999ankits@gmail.com>
2023-07-02 20:51:46 +0530

    Time: 101 ms, Memory: 17.7 MB - LeetHub

--- search-for-a-range/search-for-a-range.py
+++ search-for-a-range/search-for-a-range.py
@@ -1,14 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if nums and nums[0]<= target and nums[len(nums)-1] >= target:
-            if target-nums[0] >= 0:
-                first_pointer = 0
-                last_pointer = len(nums)-1
-                while (first_pointer < last_pointer) and (nums[first_pointer] != target or nums[last_pointer] != target):
-                    if nums[first_pointer] < target:
-                        first_pointer+=1
-                    if nums[last_pointer] > target:
-                        last_pointer-=1
-                return [first_pointer,last_pointer] if nums[first_pointer] == target else [-1,-1]
+        first,last,first_target_index = 0, len(nums)-1, -1
+        while first <= last:
+            mid = (last+first) >> 1
+            if nums[mid] == target:
+                first_target_index = mid
+                break
+            elif nums[mid] > target:
+                last = mid-1
+            else:
+                first = mid+1
+                
+        if first_target_index == -1:
+            return [-1,-1]
         
-        return [-1,-1]
+        first=first_target_index
+        while (first >=0 and nums[first] == target):
+            first-=1
+        
+        last=first_target_index
+        while (last < len(nums) and nums[last] == target):
+            last+=1
+        return [first+1,last-1]
