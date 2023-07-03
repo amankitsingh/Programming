0bafbe44a4b610cb48e060bc696676fa261d859f
Ankit Singh <1999ankits@gmail.com>
2023-07-03 19:25:02 +0530

    Update find-k-closest-elements.py

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -30,3 +30,18 @@ class Solution:
             k-=1
             
         return arr[start+1:end]
+
+# Answer 2 - binary search with less steps
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            print(x - arr[mid],arr[mid + k] - x,x - arr[mid] > arr[mid + k] - x)
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left + k]                 
+        
