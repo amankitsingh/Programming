b092566ac8033dc5684dc26cb56379612943502c
Ankit Singh <1999ankits@gmail.com>
2023-07-10 12:45:56 +0530

    Update remove-linked-list-elements.py

--- remove-linked-list-elements/remove-linked-list-elements.py
+++ remove-linked-list-elements/remove-linked-list-elements.py
@@ -3,6 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Answer 1
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if head is None:
+            return
+        if head.val == val:
+            while head:
+                if head.val == val:
+                    head = head.next
+                else:
+                    break
+        prev = head
+        temp = head
+        while temp and temp.next:
+            if temp.val == val:
+                prev.next = temp.next
+            else:
+                prev = temp
+            temp = temp.next
+        
+        if temp and temp.next is None:
+            if temp.val == val:
+                prev.next = None
+        
+        return head
+
+# Answer 2
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         if head is None:
