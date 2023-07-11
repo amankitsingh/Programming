a8035c35d5bc540a4186cd81a5c9c626ce1b29bc
Ankit Singh <1999ankits@gmail.com>
2023-07-11 16:45:02 +0530

    Update and rename list.py to double_linked_list.py

--- design-double-linked-list/double_linked_list.py
+++ design-double-linked-list/double_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.prev = prev
         self.next = nextlink
 
-class Solution:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -13,29 +13,30 @@ class Solution:
     def get(self, index):
         if index > self.size or self.size <= 0:
             return -1
-        if abs(self.size - index) < abs(index - 0):
+        if abs(self.size - index) < index:
             count = self.size - 1
             temp = self.tail
-            while temp and temp.prev and index != self.size:
+            while temp and temp.prev and count != index:
                 temp = temp.prev
                 count-=1
             return temp.val
         else:
             count = 0
             temp = self.head
-            while temp and temp.next and count!=index:
+            while temp and temp.next and count != index:
                 temp = temp.next
                 count+=1
             return temp.val
     
     def addHead(self, val):
         if self.head:
-            new_node = Node(val, None, head.next)
-            head.prev = new_node
-            head = new_node
+            new_node = Node(val, None, self.head)
+            self.head.prev = new_node
+            self.head = new_node
         else:
             new_node = Node(val)
-            head=new_node
+            self.head = new_node
+            self.tail = new_node
         self.size+=1
     def addTail(self,val):
         if self.tail:
@@ -47,7 +48,7 @@ class Solution:
             self.addHead(val)
     
     def addAtIndex(self, index, val):
-        if index > self.size:
+        if index > self.size or index < 0:
             return
         elif index == self.size:
             self.addTail(val)
@@ -55,12 +56,12 @@ class Solution:
             self.addHead(val)
         else:
             temp = self.head
+            index-=1
             while index > 0:
                 temp = temp.next
                 index-=1
             new_node = Node(val, temp, temp.next)
-            temp.next.prev = new_node
-            temp.next = new_node
+            temp.next.prev = temp.next = new_node
             self.size+=1
             
     def deleteAtIndex(self, index):
@@ -68,9 +69,30 @@ class Solution:
             return
         elif index == 0:
             self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+            else:
+                self.head.prev = None
         else:
             temp = self.head
             while index > 0:
                 temp = temp.next
+                index-=1
             temp.prev.next,temp.next.prev = temp.next, temp.prev
             self.size-=1
+    
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val, end="->")
+            current = current.next
+        print()
+
+dll = DoublyLinkedList()
+dll.addHead(2)
+dll.addTail(4)
+dll.addAtIndex(1, 3)
+dll.display()  # Output: 2 3 4
+print(dll.get(2))  # Output: 4
+dll.deleteAtIndex(1)
+dll.display()  # Out
