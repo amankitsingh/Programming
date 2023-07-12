1561f251e9618e2a840bd0b91b5f03221a7092b4
Ankit Singh <1999ankits@gmail.com>
2023-07-12 23:08:32 +0530

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,6 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Answer 1 - Time complexity O(m+n) - space complexity: O(m+n)
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        head = ListNode(-10000)
+        temp = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = ListNode(list1.val)
+                temp = temp.next
+                list1 = list1.next
+            elif list1.val == list2.val:
+                new_node = ListNode(list1.val,ListNode(list2.val))
+                temp.next = new_node
+                temp = temp.next.next
+                list1 = list1.next
+                list2 = list2.next
+            else:
+                temp.next = ListNode(list2.val)
+                temp = temp.next
+                list2 = list2.next
+        if list1:
+            while list1:
+                temp.next = ListNode(list1.val)
+                temp = temp.next
+                list1 = list1.next
+        if list2:
+            while list2:
+                temp.next = ListNode(list2.val)
+                temp = temp.next
+                list2 = list2.next
+        return head.next
+
+#Answer2 - Time complexity: O(m+n) - space complexity: O(1)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode()
