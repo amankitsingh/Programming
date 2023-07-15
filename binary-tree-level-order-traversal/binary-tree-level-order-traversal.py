94e289d89d9686050b982bbb1ef4e85ac0909aa5
Ankit Singh <1999ankits@gmail.com>
2023-07-15 19:08:39 +0530

    Update binary-tree-level-order-traversal.py

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -15,30 +15,6 @@ class Solution:
         stack.append([root])
         while stack:
             curr_node = stack.popleft()
-            temp = []
-            for i in curr_node:
-                if i.left:
-                    temp.append(i.left)
-                if i.right:
-                    temp.append(i.right)
-            if temp:
-                stack.append(temp)
-            temp = []
-            for i in curr_node:
-                temp.append(i.val)
-            answer.append(temp)
-        return answer
-
-# Answer 2
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
-            return []
-        stack = deque()
-        answer = []
-        stack.append([root])
-        while stack:
-            curr_node = stack.popleft()
             temp,result = [],[]
             for i in curr_node:
                 if i.left:
