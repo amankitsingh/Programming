3190e376a620ca94f07827435eefd3c0ebdabcb0
Ankit Singh <1999ankits@gmail.com>
2023-07-17 23:48:16 +0530

    Time: 79 ms, Memory: 17.7 MB - LeetHub

--- populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -10,21 +10,34 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        def add_neighbour(te, lev):
-            if te.left:
-                queue.append([te.left, lev+1])
-            if te.right:
-                queue.append([te.right, lev+1])
-        queue = deque()
-        if root is None:
-            return
-        queue.append((root,0))
-        while queue:
-            temp,level = queue.popleft()
-            add_neighbour(temp,level)
-            while len(queue) > 0 and queue[0][1] == level:
-                temp2,level2 = queue.popleft()
-                add_neighbour(temp2,level2)
-                temp.next = temp2
-                temp = temp2
+        if root and root.left and root.right:
+            root.left.next = root.right
+            if root.right:
+                temp = root.next
+                while temp and temp.next and (temp.left is None and temp.right is None):
+                    temp=temp.next
+                if temp:
+                    root.right.next = temp.left or temp.right
+        else:
+            if root and root.next and (root.left or root.right):
+                if root.left:
+                    temp = root.next
+                    while temp.next and (temp.left is None and temp.right is None):
+                        temp=temp.next
+                    if temp.left:
+                        root.left.next = temp.left
+                    elif temp.right:
+                        root.left.next = temp.right
+                else:
+                    temp = root.next
+                    while temp.next and (temp.left is None and temp.right is None):
+                        temp=temp.next
+                    if temp.left:
+                        root.right.next = temp.left
+                    elif temp.right:
+                        root.right.next = temp.right
+        if root:
+            self.connect(root.right)
+            self.connect(root.left)
+            
         return root
