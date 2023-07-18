1fb99c58ac49136e22746327f2607a7804d4a0c9
Ankit Singh <1999ankits@gmail.com>
2023-07-18 23:35:06 +0530

    Update serialize-and-deserialize-binary-tree.py

--- serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -5,6 +5,49 @@
 #         self.left = None
 #         self.right = None
 
+# Answer 1 - Time complexity O(n), Space complexity O(n)
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        stack = deque()
+        answer = []
+        stack.append(root)
+        while stack:
+            temp = stack.popleft()
+            if temp:
+                answer.append(str(temp.val))
+                stack.append(temp.left if temp.left else None)
+                stack.append(temp.right if temp.right else None)
+            else:
+                answer.append("null")
+        return ",".join(answer)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        nodes = [None if val == "null" else TreeNode(int(val))
+             for val in data.split(',')]
+        offset = 0
+        for i in range(len(nodes)):
+            if nodes[i]:
+                nodes[i].left = nodes[i+1+offset] if i+1+offset < len(nodes) else None
+                nodes[i].right = nodes[i+2+offset] if i+2+offset < len(nodes) else None
+                offset+=1
+
+            else:
+                offset-=1
+        return nodes[0]
+
+# Answer 2 - Time complexity O(n), Space complexity O(1)
 class Codec:
 
     def serialize(self, root):
