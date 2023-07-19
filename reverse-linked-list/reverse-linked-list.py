c733fa90bbe64e20369a73f6f4b9746f2298f415
Ankit Singh <1999ankits@gmail.com>
2023-07-19 23:10:37 +0530

    Update reverse-linked-list.py

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -3,6 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Answer 1 - Iterative
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_head = head
+        while head and head.next:
+            temp = head.next
+            head.next = temp.next
+            temp.next = new_head
+            new_head = temp
+        return new_head
+#Answer 2 - Recursive
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
