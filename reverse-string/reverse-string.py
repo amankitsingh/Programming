65de81e760b35f46fd4450bbdca8617dcc6ebb15
Ankit Singh <1999ankits@gmail.com>
2023-07-19 18:03:22 +0530

    Update reverse-string.py

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,3 +1,4 @@
+# Answer 1 - Iterative
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         j = len(s)-1
@@ -6,3 +7,14 @@ class Solution:
             s[i],s[j] = s[j],s[i]
             j-=1
         return s
+# Answer 2 - Recursive
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def helper(low,high):
+            if low>high:
+                return
+            s[low],s[high]=s[high],s[low]
+            helper(low+1,high-1)
+        helper(0,len(s)-1)
+        
+        
