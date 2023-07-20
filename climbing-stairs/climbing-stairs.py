9ded20f65a5135b7a9b61d3f2cd732a49d4ba607
Ankit Singh <1999ankits@gmail.com>
2023-07-20 15:40:49 +0530

    Time: 39 ms, Memory: 16.3 MB - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,13 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {}
-        def helper(N):
-            if N in cache:
-                return cache[N]
-            if N<=3:
-                result = N
-            else:
-                result = helper(N-1)+ helper(N-2)
-            cache[N] = result
-            return result
-        return helper(n)
+        memo = {1:1,2:2}
+        for i in range(3,n+1):
+            memo[i] = memo[i-1]+memo[i-2]
+        return memo[n]
