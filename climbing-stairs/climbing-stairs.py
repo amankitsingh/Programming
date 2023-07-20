0849e489b9955c4ccd87bf20346a747fa121f670
Ankit Singh <1999ankits@gmail.com>
2023-07-20 15:41:57 +0530

    Update climbing-stairs.py

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,5 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        cache = {}
+        def helper(N):
+            if N in cache:
+                return cache[N]
+            if N<=3:
+                result = N
+            else:
+                result = helper(N-1)+ helper(N-2)
+            cache[N] = result
+            return result
+        return helper(n)
+        
+class Solution:
+    def climbStairs(self, n: int) -> int:
         memo = {1:1,2:2}
         for i in range(3,n+1):
             memo[i] = memo[i-1]+memo[i-2]
