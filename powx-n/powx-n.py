8e1b8bb258aa5d362efcc4a5cf9350b80178bd5d
Ankit Singh <1999ankits@gmail.com>
2023-07-20 17:07:14 +0530

    Update powx-n.py

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,3 +1,4 @@
+# Answer1 - Iterative
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if x == 1:
@@ -14,3 +15,23 @@ class Solution:
             n//=2
                 
         return result if temp_n > 0 else 1/result 
+
+# Answer 2 - Recursive
+class Solution:
+    def binaryExp(self, x: float, n: int) -> float:
+        # Base case, to stop recursive calls.
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1.0 / self.binaryExp(x, -1 * n)
+       
+        # Perform Binary Exponentiation.
+        # If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if n % 2 == 1:
+            return x * self.binaryExp(x * x, (n - 1) // 2)
+        # Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else:
+            return self.binaryExp(x * x, n // 2)
+
+    def myPow(self, x: float, n: int) -> float:
+        return self.binaryExp(x, n)
