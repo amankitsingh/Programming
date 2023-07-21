e846c5f00e73fcc7a1e69a2cd321b9b0268deb82
Ankit Singh <1999ankits@gmail.com>
2023-07-21 18:33:26 +0530

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -66,3 +66,15 @@ class Solution:
             temp = temp.next
             list2 = list2.next
         return head.next
+
+# Answer 3 - Recursive Time complexity O(m+n) - Space complexity O(1)
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:        
+        if not list1: return list2
+        if not list2: return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
