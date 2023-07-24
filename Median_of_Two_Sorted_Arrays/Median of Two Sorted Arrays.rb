165fef343a4c159be1f8f32abdbf0aa2b241c75b
Ankit Singh <1999ankits@gmail.com>
2023-07-24 16:53:32 +0530

    Create Median of Two Sorted Arrays.rb

---
+++ Median_of_Two_Sorted_Arrays/Median of Two Sorted Arrays.rb
@@ -0,0 +1,16 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        result,i,j = [],0,0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                result.append(nums1[i])
+                i+=1
+            else:
+                result.append(nums2[j])
+                j+=1
+        mid = len(result)//2
+        print(result,mid)
+        if len(result)%2 == 0:
+            return (result[mid-1]+result[mid])/2
+        else:
+            return float(result[mid])
