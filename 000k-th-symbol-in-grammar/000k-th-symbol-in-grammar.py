7b3805043592b591564ade1c4e80bcf24b29c3f0
Ankit Singh <1999ankits@gmail.com>
2023-07-24 16:13:49 +0530

    Update 000k-th-symbol-in-grammar.py

--- 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
@@ -1,5 +1,10 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
+        return bin(k-1).count('1')%2
+
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
         if n == 1:
             return 0
         if n == 2:
