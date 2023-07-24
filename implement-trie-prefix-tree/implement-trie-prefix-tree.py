12a86b10f3af23af9fbaded70adf995e37cb479f
Ankit Singh <1999ankits@gmail.com>
2023-07-25 01:42:05 +0530

    Update implement-trie-prefix-tree.py

--- implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,3 +1,4 @@
+# Better memory
 class Trie:
 
     def __init__(self):
@@ -27,6 +28,35 @@ class Trie:
             head = head.children[s]
         return True
 
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+    def insert(self, word: str) -> None:
+        head = self
+        for s in word:
+            if s not in head.children:
+                head.children[s] = Trie()
+            head = head.children[s]
+        head.word = True
+            
+    def search(self, word: str) -> bool:
+        head = self
+        for s in word:
+            if s not in head.children:
+                return False
+            head = head.children[s]
+        return head.word
+    
+    def startsWith(self, prefix: str) -> bool:
+        head = self
+        for s in prefix:
+            if s not in head.children:
+                return False
+            head = head.children[s]
+        return True
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
