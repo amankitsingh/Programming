b59df6d064fcdd8aece643a7988f40252b25a7a7
Ankit Singh <1999ankits@gmail.com>
2023-07-25 16:25:00 +0530

    Time: 46 ms, Memory: 16.4 MB - LeetHub

--- 0map-sum-pairs/0map-sum-pairs.py
+++ 0map-sum-pairs/0map-sum-pairs.py
@@ -1,17 +1,34 @@
 class MapSum:
 
     def __init__(self):
-        self.dic = {}
+        self.children = {}
+        self.val = 0
+        self.isWord = False
 
     def insert(self, key: str, val: int) -> None:
-        self.dic[key] = val
+        head = self
+        for s in key:
+            if s not in head.children:
+                head.children[s] = MapSum()
+            head = head.children[s]
+        head.isWord = True
+        head.val = val
 
     def sum(self, prefix: str) -> int:
+        head = self
+        for s in prefix:
+            if s not in head.children:
+                return 0
+            head = head.children[s]
         total = 0
-        for k,v in self.dic.items():
-            length = len(prefix)
-            if prefix == k[:length]:
-                total+=v
+        queue = deque()
+        queue.append(head)
+        while queue:
+            temp = queue.popleft()
+            if temp.isWord:
+                total+=temp.val
+            for c in temp.children:
+                queue.append(temp.children[c])
         return total
 
 
