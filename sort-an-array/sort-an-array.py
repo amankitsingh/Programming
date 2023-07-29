2eba52a993a2d140e33dccfa2b33421ef1193676
Ankit Singh <1999ankits@gmail.com>
2023-07-30 00:46:37 +0530

    Time: 1477 ms, Memory: 25 MB - LeetHub

--- sort-an-array/sort-an-array.py
+++ sort-an-array/sort-an-array.py
@@ -2,21 +2,20 @@ class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         if len(nums) ==1:
             return nums
-        if len(nums) == 2:
-            if nums[0] > nums[1]:
-                nums[0],nums[1] = nums[1],nums[0]
-            return nums
-        middle = len(nums)//2
-        first_half = self.sortArray(nums[:middle])
-        second_half = self.sortArray(nums[middle:])
-        i,j,answer = 0,0,[]
-        while i < len(first_half) and j < len(second_half):
-            if first_half[i] <= second_half[j]:
-                answer.append(first_half[i])
+        mid = len(nums)//2
+        left = self.sortArray(nums[mid:])
+        right = self.sortArray(nums[:mid])
+        return self.merge(left,right)
+    
+    def merge(self, left, right):
+        i,j, result = 0,0, []
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
                 i+=1
             else:
-                answer.append(second_half[j])
+                result.append(right[j])
                 j+=1
-        answer.extend(first_half[i:])
-        answer.extend(second_half[j:])
-        return answer
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
