f12ee6f87a55c3c4f2c4e3fd7fbf57876f22b326
Ankit Singh <1999ankits@gmail.com>
2023-08-04 00:05:56 +0530

    Update generate-parentheses.py

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -16,4 +16,23 @@ class Solution:
                 stack.pop()
         backtrace(0,0)
         return result
+
+class Solution:
+    def generateParenthesis(self, n):
+        if not n:
+            return []
+        left, right, ans = n, n, []
+        self.dfs(left,right, ans, "")
+        return ans
+
+    def dfs(self, left, right, ans, string):
+        if right < left:
+            return
+        if not left and not right:
+            ans.append(string)
+            return
+        if left:
+            self.dfs(left-1, right, ans, string + "(")
+        if right:
+            self.dfs(left, right-1, ans, string + ")")
                 
