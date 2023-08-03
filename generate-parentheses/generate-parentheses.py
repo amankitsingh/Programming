97f66fa06e613b435a742b5cc5c7b299a560ec92
Ankit Singh <1999ankits@gmail.com>
2023-08-04 00:05:37 +0530

    Time: 46 ms, Memory: 16.6 MB - LeetHub

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -1,18 +1,19 @@
 class Solution:
     def generateParenthesis(self, n):
-        if not n:
-            return []
-        left, right, ans = n, n, []
-        self.dfs(left,right, ans, "")
-        return ans
-
-    def dfs(self, left, right, ans, string):
-        if right < left:
-            return
-        if not left and not right:
-            ans.append(string)
-            return
-        if left:
-            self.dfs(left-1, right, ans, string + "(")
-        if right:
-            self.dfs(left, right-1, ans, string + ")")
+        result = []
+        stack = []
+        def backtrace(openp, closep):
+            if openp == closep == n:
+                result.append("".join(stack))
+                return
+            if openp < n:
+                stack.append("(")
+                backtrace(openp+1,closep)
+                stack.pop()
+            if closep < openp:
+                stack.append(")")
+                backtrace(openp, closep+1)
+                stack.pop()
+        backtrace(0,0)
+        return result
+                
