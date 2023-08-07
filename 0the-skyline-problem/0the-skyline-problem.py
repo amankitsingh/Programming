0bec66f0343d2f7c878fb4b53e6c03916e143ab2
Ankit Singh <1999ankits@gmail.com>
2023-08-07 19:17:47 +0530

    Time: 299 ms, Memory: 21.2 MB - LeetHub

---
+++ 0the-skyline-problem/0the-skyline-problem.py
@@ -0,0 +1,26 @@
+from heapq import heappop, heappush, heapify
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        node_list = []
+        for i in range(len(buildings)):
+            left,right,height = buildings.pop(0)
+            node_list.append([left,-height])
+            node_list.append([right,height])
+        node_list.sort()        
+        result = []
+        curheight = 0
+        max_heap = [0]
+        for x,height in node_list:
+            if height < 0:
+                max_heap.append(-1*height)
+                max_heap.sort()
+            else:
+                max_heap.remove(height)
+            max_height = max_heap[-1]
+            if curheight != max_height:
+                result.append([x,max_height])
+                curheight = max_height
+        return result
+            
+        
+        
