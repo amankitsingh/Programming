9113516afc69c1a17f4788007b0cbb604c8ea654
Ankit Singh <1999ankits@gmail.com>
2023-10-08 13:19:31 +0530

    Update palindrome-linked-list.py

--- palindrome-linked-list/palindrome-linked-list.py
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -1,3 +1,24 @@
+## Better approach
+#class ListNode:
+#	def __init__(self, val=0, next=None):
+#		self.val = val
+#		self.next = next
+
+class Solution:
+	def isPalindrome(self, head):
+    rev = None
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        rev, rev.next, slow = slow, rev, slow.next
+    if fast:
+        slow = slow.next
+    while rev and rev.val == slow.val:
+        slow = slow.next
+        rev = rev.next
+    return not rev
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
