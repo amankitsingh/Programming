701e8e18857d95e4a7f378975225a655d7a54d6c
Ankit Singh <1999ankits@gmail.com>
2023-10-13 16:23:29 +0530

    Time: 1517 ms, Memory: 30.3 MB - LeetHub

---
+++ 0kth-largest-element-in-an-array/0kth-largest-element-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) ==1:
+            return nums
+        mid = len(nums)//2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+        return self.merge(left,right)
+    
+    def merge(self, left, right):
+        i,j, result = 0,0, []
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i+=1
+            else:
+                result.append(right[j])
+                j+=1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = self.sortArray(nums)
+        return nums[len(nums)-k]
