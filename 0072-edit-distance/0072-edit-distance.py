ab419517b02b109aa2d6eb1cd27642802226bce9
Ankit Singh <1999ankits@gmail.com>
2023-11-19 18:16:33 +0530

    Time: 149 ms (16.20%), Space: 20.1 MB (28.56%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,24 +3,22 @@ class Solution:
         n = len(word1)
         m = len(word2)
         
-        dp =[[-1]*(m+1) for _ in range(n+1)]
+        dp =[[0]*(m+1) for _ in range(n+1)]
         
-        def helper(index1, index2):
-            if index1 < 0:
-                return index2+1
-            if index2 < 0:
-                return index1+1
-            
-            if dp[index1][index2] != -1:
-                return dp[index1][index2]
-            
-            if word1[index1] == word2[index2]:
-                dp[index1][index2] = helper(index1-1,index2-1)
-            else:
-                dp[index1][index2] = 1 + min(helper(index1-1,index2),min(helper(index1-1,index2-1),
-                                                                        helper(index1,index2-1)))
-            return dp[index1][index2]
+        for i in range(n+1):
+            dp[i][0] = i
+        
+        for j in range(m+1):
+            dp[0][j] = j
+        
+        for index1 in range(1, n+1):
+            for index2 in range(1, m+1):
+                if word1[index1-1] == word2[index2-1]:
+                    dp[index1][index2] = dp[index1-1][index2-1]
+                else:
+                    dp[index1][index2] = 1 + min(dp[index1-1][index2],min(dp[index1-1][index2-1],
+                                                                            dp[index1][index2-1]))
         
                                             
                                              
-        return helper(n-1,m-1)
+        return dp[n][m]
