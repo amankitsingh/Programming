3e8f5e4354c21f5be39317b8a6a0bf98d93017ea
Ankit Singh <1999ankits@gmail.com>
2023-11-19 18:25:30 +0530

    Time: 119 ms (67.04%), Space: 16.3 MB (94.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -4,10 +4,10 @@ class Solution:
         m = len(word2)
         
         prev = [j for j in range(m + 1)]
-        curr = [0 for _ in range(m + 1)]
         
         
         for index1 in range(1, n+1):
+            curr = [0]*(m+1)
             curr[0] = index1
             for index2 in range(1, m+1):
                 if word1[index1-1] == word2[index2-1]:
