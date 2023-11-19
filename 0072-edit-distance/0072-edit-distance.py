9fefebabe8a2af8c87309867589a363a46b728d6
Ankit Singh <1999ankits@gmail.com>
2023-11-19 18:20:10 +0530

    Time: 130 ms (43.76%), Space: 16.2 MB (99.16%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,22 +3,19 @@ class Solution:
         n = len(word1)
         m = len(word2)
         
-        dp =[[0]*(m+1) for _ in range(n+1)]
+        prev = [j for j in range(m + 1)]
+        curr = [0 for _ in range(m + 1)]
         
-        for i in range(n+1):
-            dp[i][0] = i
-        
-        for j in range(m+1):
-            dp[0][j] = j
         
         for index1 in range(1, n+1):
+            curr[0] = index1
             for index2 in range(1, m+1):
                 if word1[index1-1] == word2[index2-1]:
-                    dp[index1][index2] = dp[index1-1][index2-1]
+                    curr[index2] = prev[index2-1]
                 else:
-                    dp[index1][index2] = 1 + min(dp[index1-1][index2],min(dp[index1-1][index2-1],
-                                                                            dp[index1][index2-1]))
-        
+                    curr[index2] = 1 + min(prev[index2],min(prev[index2-1],curr[index2-1]))
+            #prev,curr = curr,prev
+            prev = curr[:]
                                             
                                              
-        return dp[n][m]
+        return prev[m]
