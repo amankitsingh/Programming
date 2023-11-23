028837a42db6dab06d2fbd9be11c678ce51c3ca6
Ankit Singh <1999ankits@gmail.com>
2023-11-23 18:56:52 +0530

    Update word-ladder-ii.py

--- Word Ladder II - GFG/word-ladder-ii.py
+++ Word Ladder II - GFG/word-ladder-ii.py
@@ -1,5 +1,7 @@
-#User function Template for python3
-
+### Answer 1
+### Time complexity - O(N*M*26) - n - queue, m- work
+### Space complexity - O(N)
+from collections import deque 
 class Solution:
     def findSequences(self, beginWord, endWord, wordList):
         n = beginWord
@@ -40,46 +42,3 @@ class Solution:
                           
                 
         return result
-
-
-#{ 
- # Driver Code Starts
-
-from collections import deque 
-import functools
-
-def comp(a, b):
-    x = ""
-    y = ""
-    for i in a:
-        x += i 
-    for i in b:
-        y += i
-    if x>y:
-        return 1
-    elif y>x:
-        return -1 
-    else:
-        return 0
-
-if __name__ == '__main__':
-    T=int(input())
-    for tt in range(T):
-        n = int(input())
-        wordList = []
-        for i in range(n):
-            wordList.append(input().strip())
-        startWord = input().strip()
-        targetWord = input().strip()
-        obj = Solution()
-        ans = obj.findSequences(startWord, targetWord, wordList)
-        if len(ans)==0:
-            print(-1)
-        else:
-            ans = sorted(ans, key=functools.cmp_to_key(comp))
-            for i in range(len(ans)):
-                for j in range(len(ans[i])):
-                    print(ans[i][j],end=" ")
-                print()
-
-# } Driver Code Ends
