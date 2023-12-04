b3bfc5d4a65d3f85b1ec99f162dc778ca7e3f740
Ankit Singh <1999ankits@gmail.com>
2023-12-04 16:10:31 +0530

    Update 0078-subsets.py

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,3 +1,4 @@
+### Time complexity - O(N*N), Space complexity - O(2*N)
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
@@ -15,3 +16,21 @@ class Solution:
         for k in range(n+1):
             backtrack(0, [], k)
         return subset
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        subset = []
+        def combi(start, comb, k):
+            if len(comb) == k:
+                subset.append(comb[:])
+                return
+            
+            for i in range(start, n+1):
+                comb.append(nums[start])
+                backtrack(i+1, comb, k)
+                comb.pop()
+            
+        for k in range(2, n):
+            combi(1, [], k)
