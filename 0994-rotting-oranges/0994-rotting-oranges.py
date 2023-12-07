7aecec36ca1d9165b976e9fc6f4cd5530f322c3e
Ankit Singh <1999ankits@gmail.com>
2023-12-08 00:08:34 +0530

    Time: 60 ms (36.31%), Space: 16.3 MB (40.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,25 +3,28 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         queue = deque()
+        total = days = count = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    queue.append((i,j,0))
+                    queue.append((i,j))
+                if grid[i][j] != 0:
+                    total+=1
 
         maxi = 0
         while queue:
-            i,j,minute = queue.popleft()
-            maxi = max(maxi, minute)
-            grid[i][j] = -1
-            directions = [0,1,0,-1,0]
-            for k in range(len(directions)-1):
-                di,dj = i+directions[k], j+directions[k+1]
-                if 0<= di < n and 0<= dj < m and grid[di][dj] == 1:
-                    if (di,dj,minute) not in queue:
-                        queue.append((di,dj,minute+1))
+            lenp = len(queue)
+            count+=lenp
+            while lenp > 0:
+                lenp-=1
+                i,j = queue.popleft()
+                directions = [0,1,0,-1,0]
+                for k in range(len(directions)-1):
+                    di,dj = i+directions[k], j+directions[k+1]
+                    if 0<= di < n and 0<= dj < m and grid[di][dj] == 1:
+                        grid[di][dj] = 2
+                        queue.append((di,dj))
+            if queue:
+                days+=1
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    return -1
-        return maxi
+        return days if total == count else -1
