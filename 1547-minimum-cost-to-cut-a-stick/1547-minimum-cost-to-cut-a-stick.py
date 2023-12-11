1564cdcc7a8c43fd6793f11a1db9173fc949e63a
Ankit Singh <1999ankits@gmail.com>
2023-12-11 15:10:12 +0530

    Time: 1164 ms (43.26%), Space: 17.7 MB (63.13%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = [0] + sorted(cuts) +[n]
+        m = len(cuts)
+        dp = [[-1]*(m+1) for _ in range(m+1)]
+        def minicut(i,j):
+            if i >= j or j-i == 1:
+                return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            mini = float("inf")
+            for k in range(i+1,j):
+                mini = min(minicut(i,k)+minicut(k,j)+(cuts[j]-cuts[i]),mini)
+            dp[i][j] = mini
+            return dp[i][j]
+        return minicut(0,m-1)
