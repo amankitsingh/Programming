2526dc2395e8e950ddc7f38202c509558bb78cc3
Ankit Singh <1999ankits@gmail.com>
2023-12-12 19:31:02 +0530

    Time: 123 ms (11.86%), Space: 24.9 MB (5.14%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -1,30 +1,5 @@
 class Solution:
-    def parseBoolExpr(self, expression: str) -> bool:
-        operator = ["&","|","!"]
-        operands = ["t","f"]
-        
-        stack = []
-        
-        for i in expression:
-            if i == ")":
-                temp = []
-                temp.append(stack.pop())
-                for k in range(len(stack)-1, 0,-1):
-                    if stack[k] != "(":
-                        temp.append(stack.pop())
-                    else:
-                        stack.pop() #remove the first open bracket
-                        break
-                exper = stack.pop()
-                if exper == "!":
-                    stack.append("f" if "t" in temp else "t")
-                elif exper == "&":
-                    stack.append("f" if "f" in temp else "t")
-                elif exper == "|":
-                    stack.append("t" if "t" in temp else "f")
-            elif i != ",":
-                stack.append(i)
-        
-        return stack[0] == "t"
+    def parseBoolExpr(self, expression: str, t=True, f=False) -> bool:
+        return eval(expression.replace('!', 'not |').replace('&(', 'all([').replace('|(', 'any([').replace(')', '])'))
                 
             
