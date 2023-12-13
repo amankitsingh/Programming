8990f53abb4af10b617fb3a9dbd7e8eaa8b0132a
Ankit Singh <1999ankits@gmail.com>
2023-12-13 14:49:09 +0530

    Update 0132-palindrome-partitioning-ii.py

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,3 +1,30 @@
+### Answer 1 - Recursion + Memoization
+### Time complexity - O(N*N*N), Space complexity - O(N*N) + O(N)
+class Solution:
+    def minCut(self, string: str) -> int:
+        if string ==  string[::-1]:
+            return 0
+
+        n = len(string)
+        dp = [-1]*n
+
+        def palipart(i):
+            if i == n:
+                return 0
+
+            if dp[i]!= -1:
+                return dp[i]
+
+            mini = float("inf")
+            for j in range(i,n):
+                if string[i:j] == string[j:i:-1]:
+                    mini = min(mini, 1 + palipart(j+1))
+            dp[i] = mini
+            return dp[i]
+        return palipart(0)-1
+        
+### Answer 2 - Bottom-up approach
+### Time complexity - O(N*N), Space complexity - O(N*N)
 class Solution:
     def minCut(self, string: str) -> int:
         if string ==  string[::-1]:
