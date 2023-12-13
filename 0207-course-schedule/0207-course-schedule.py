26be6632a2f3cb2499ffcabec9fc8ff570928d3b
Ankit Singh <1999ankits@gmail.com>
2023-12-13 23:47:15 +0530

    Time: 96 ms (73.15%), Space: 19.2 MB (30.25%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        taken = set()
+
+        for node_from,node_to in prerequisites:
+            adj[node_from].append(node_to)
+    
+        def dfs(node):
+            if not adj[node]:
+                return True
+            if node in taken:
+                return False
+            taken.add(node)
+            for it in adj[node]:
+                if not dfs(it):
+                    return False
+            adj[node] = []
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        else:
+            return True
