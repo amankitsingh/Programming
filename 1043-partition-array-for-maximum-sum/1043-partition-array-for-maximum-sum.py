aa6276cf6ae5d10a089273cb7874379b1843170a
Ankit Singh <1999ankits@gmail.com>
2023-12-14 15:18:49 +0530

    Update 1043-partition-array-for-maximum-sum.py

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,3 +1,29 @@
+### Answer 1 - Recursion + Memoization
+### Time complexity - O(N*K), Space complexity - O(N) + O(N) ~ O(N)
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+
+        dp = [-1]*(n)
+        def findmaxsum(i):
+            if i == n:
+                return 0
+            if dp[i]!=-1:
+                return dp[i]
+            maxi = float("-inf")
+            length,maxElement = 0,float("-inf")
+            for j in range(i, min(i+k,n)):
+                length+=1
+                maxElement = max(arr[j], maxElement)
+                sump = maxElement*length + findmaxsum(j+1)
+                maxi = max(maxi,sump)
+            dp[i] = maxi
+            return maxi
+
+        return findmaxsum(0)
+
+### Answer 2 - Bottom up approach
+### Time complexity - O(N*K), Space complexity - O(N)
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
