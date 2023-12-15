8cc29c27f8b22d309388ea630c2be4a913f287f0
Ankit Singh <1999ankits@gmail.com>
2023-12-15 14:50:27 +0530

    Time: 255 ms (58.23%), Space: 17.8 MB (31.33%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        def largesthistogram(arr):
+            stack = []
+            result = 0
+            for i,val in enumerate(arr):
+                start = i
+                while stack and stack[-1][1] > val:
+                    index, height = stack.pop()
+                    result = max(result, height*(i-index))
+                    start = index
+                stack.append((start, val))
+            
+            for i,v in stack:
+                result = max(result, v*(len(arr)-i))
+            
+            return result
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        height = [0]*m
+        maxi = 0
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == "1":
+                    height[j]+=1
+                else:
+                    height[j] = 0
+            maxi = max(maxi, largesthistogram(height))
+        
+        return maxi
+            
+                    
