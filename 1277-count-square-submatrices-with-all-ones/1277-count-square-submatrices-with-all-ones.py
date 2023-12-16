13b31631a7349c5070e190c7c84e67f7ad7487fa
Ankit Singh <1999ankits@gmail.com>
2023-12-16 15:27:30 +0530

    Update 1277-count-square-submatrices-with-all-ones.py

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,5 +1,5 @@
 ### Answer 1
-### Time complexity - O(3*N*M + N + M)~O(N*M), Space complexity -  O(N*M)
+### Time complexity - O(N*M + 3*N + M)~O(N*M), Space complexity -  O(N*M)
 class Solution:
     def countSquares(self, arr: List[List[int]]) -> int:
         n = len(arr)
@@ -10,12 +10,10 @@ class Solution:
 
         for i in range(1,n):
             for j in range(1,m):
-                if arr[i][j] == 0:
-                    dp[i][j] = 0
-                else:
+                if arr[i][j] == 1:
                     dp[i][j] = 1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
 
         result=0
-        for i in range(n):
-            result+=sum(dp[i])
+        for i in dp:
+            result+=sum(i)
         return result
