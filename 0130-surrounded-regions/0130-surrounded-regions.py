237ed17e3c801344b35651dab03e99702d79fff8
Ankit Singh <1999ankits@gmail.com>
2023-12-16 18:17:42 +0530

    Time: 129 ms (89.42%), Space: 18.4 MB (34.06%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        n = len(board)
+        m = len(board[0])
+        
+        
+        def change_the_adj(x,y):
+            board[x][y]="P"
+            directions = [0,1,0,-1,0]
+            for k in range(4):
+                dx,dy = x+directions[k],y+directions[k+1]
+                if 0<= dx < n and 0<= dy < m and board[dx][dy] == "O":
+                    change_the_adj(dx,dy)
+
+                    
+        for i in range(n):
+            for j in range(m):
+                if (i==0 or j==0 or j==m-1 or i==n-1) and board[i][j] == "O":
+                    change_the_adj(i,j)
+        
+        
+        for i in range(n):
+            for j in range(m):
+                if 0<= i < n and 0<= j < m and board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "P":
+                    board[i][j] = "O"
