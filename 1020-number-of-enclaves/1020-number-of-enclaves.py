d8e7a61f0031e701fca514df4c41958cd9ac2833
Ankit Singh <1999ankits@gmail.com>
2023-12-16 18:42:38 +0530

    Update 1020-number-of-enclaves.py

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -25,3 +25,37 @@ class Solution:
                 if 0<=i<n and 0<=j<m and grid[i][j] == 1:
                     count+=1
         return count
+        
+### Answer 2
+### just another way to code
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        def change_the_value(x,y):
+            if 0<=x<n and 0<=y<m and grid[x][y] == 1:
+                grid[x][y] = 0
+                directions = [0,1,0,-1,0]
+                for k in range(4):
+                    dx,dy = x+directions[k],y+directions[k+1]
+                    if 0<=dx<n and 0<=dy<m and grid[dx][dy] == 1:
+                        change_the_value(dx,dy)
+
+
+                    
+        for i in range(n):
+            change_the_value(i,0)
+            change_the_value(i,m-1)
+            
+        for i in range(m):
+            change_the_value(0,i)
+            change_the_value(n-1,i)
+            
+      
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    count+=1
+        return count
