a15ae8c681147a4416b7a53a5feedda2e7bef8c7
Ankit Singh <1999ankits@gmail.com>
2023-12-19 13:52:28 +0530

    Update topological-sort.py

--- Topological sort - GFG/topological-sort.py
+++ Topological sort - GFG/topological-sort.py
@@ -17,3 +17,32 @@ class Solution:
                 dfs(i)
                 
         return result[::-1]
+
+
+### Answer 2 - BFS = kahn's algorithm
+### Time complexity - O(V+V+V*E)~O(V*E), Space complexity - O(2*V)~O(V)
+from collections import deque
+class Solution:
+    
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        queue = deque()
+        indegree = [0]*V
+    
+        for node in range(V):
+            for v in adj[node]:
+                indegree[v]+=1
+    
+        for i in range(V):
+            if indegree[i] == 0:
+                queue.append(i)
+    
+        result = []
+        while queue:
+            vertex = queue.popleft()
+            result.append(vertex)
+            for node in adj[vertex]:
+                indegree[node]-=1
+                if indegree[node] == 0:
+                    queue.append(node)
+        return result
