3ea8e47c209bcf9285534689124a650d7ddbb0a6
Ankit Singh <1999ankits@gmail.com>
2023-12-20 17:41:37 +0530

    Time: 514 ms (100.00%), Space: 25.1 MB (7.38%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,26 +1,31 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        queue = deque()
-        V = len(graph)
-        indegree = [0]*V
-    
-        graphrev = defaultdict(list)
-        for node in range(V):
-            for v in graph[node]:
-                graphrev[v].append(node)
-                indegree[node]+=1
-    
-        for i in range(V):
-            if indegree[i] == 0:
-                queue.append(i)
-    
+        n = len(graph)
         result = []
-        while queue:
-            vertex = queue.popleft()
-            result.append(vertex)
-            for node in graphrev[vertex]:
-                indegree[node]-=1
-                if indegree[node] == 0:
-                    queue.append(node)
-                    
+        visited = [0]*n
+        pathVisited = visited[:]
+        
+        def dfs(node):
+            if pathVisited[node] == 1:
+                return True
+            if visited[node]:
+                return False
+            
+            pathVisited[node] = 1
+            visited[node] = 1
+            for edge in graph[node]:
+                if dfs(edge):
+                    return True
+            pathVisited[node] = 0
+            return False
+
+        for i in range(n):
+            if visited[i] == 0:
+                dfs(i)
+        
+        for i in range(n):
+            if pathVisited[i] == 0:
+                result.append(i)
+
+
         return sorted(result)
