05766b5758ff50c4c6ecad9e844fbca9db8cf628
Ankit Singh <1999ankits@gmail.com>
2023-12-20 17:43:50 +0530

    Update 0802-find-eventual-safe-states.py

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,3 +1,34 @@
+### Answer 1 - BFS - reverse kahn's algorithm
+### Time complexity - O(V*E), Space complexity - O(V+Q) ~ O(V)
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        queue = deque()
+        V = len(graph)
+        indegree = [0]*V
+    
+        graphrev = defaultdict(list)
+        for node in range(V):
+            for v in graph[node]:
+                graphrev[v].append(node)
+                indegree[node]+=1
+    
+        for i in range(V):
+            if indegree[i] == 0:
+                queue.append(i)
+    
+        result = []
+        while queue:
+            vertex = queue.popleft()
+            result.append(vertex)
+            for node in graphrev[vertex]:
+                indegree[node]-=1
+                if indegree[node] == 0:
+                    queue.append(node)
+                    
+        return sorted(result)
+
+### Answer 2 - DFS
+### Time complexity - O(V*E), Space complexity - O(2*V + V)~O(V) auxiliary stack and visited and pathVisited array
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
