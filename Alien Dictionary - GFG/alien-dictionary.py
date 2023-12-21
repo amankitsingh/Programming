b915304d93812e774960e179fbad6e7b3f811c3e
Ankit Singh <1999ankits@gmail.com>
2023-12-21 15:08:56 +0530

    Update alien-dictionary.py

--- Alien Dictionary - GFG/alien-dictionary.py
+++ Alien Dictionary - GFG/alien-dictionary.py
@@ -1,9 +1,10 @@
-#User function Template for python3
+### Answer 1 - Using Kahn's algorithm 
+### Time complexity - O(N*M + K + K + Q) ~ O(N*M), Space complexity - O(K + N + N)~O(N)
+### Here M is the length of the largest word in the array, Q is the queue
 from collections import defaultdict,deque
 class Solution:
     def findOrder(self,alien_dict, N, K):
         topo = defaultdict(list)
-        #N = len(alien_dict)
         indegree = [0]*K
         for i in range(N-1):
             for j in range(min(len(alien_dict[i]),len(alien_dict[i+1]))):
@@ -32,53 +33,3 @@ class Solution:
                     queue.append(adj_node)
         
         return result
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-class sort_by_order:
-    def __init__(self,s):
-        self.priority = {}
-        for i in range(len(s)):
-            self.priority[s[i]] = i
-    
-    def transform(self,word):
-        new_word = ''
-        for c in word:
-            new_word += chr( ord('a') + self.priority[c] )
-        return new_word
-    
-    def sort_this_list(self,lst):
-        lst.sort(key = self.transform)
-
-if __name__ == '__main__':
-    t=int(input())
-    for _ in range(t):
-        line=input().strip().split()
-        n=int(line[0])
-        k=int(line[1])
-        
-        alien_dict = [x for x in input().strip().split()]
-        duplicate_dict = alien_dict.copy()
-        ob=Solution()
-        order = ob.findOrder(alien_dict,n,k)
-        s = ""
-        for i in range(k):
-            s += chr(97+i)
-        l = list(order)
-        l.sort()
-        l = "".join(l)
-        if s != l:
-            print(0)
-        else:
-            x = sort_by_order(order)
-            x.sort_this_list(duplicate_dict)
-            
-            if duplicate_dict == alien_dict:
-                print(1)
-            else:
-                print(0)
-
-
-# } Driver Code Ends
