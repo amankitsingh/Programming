4bdd92009c5dabe5141cbd30bb45cfcb71bb849d
Ankit Singh <1999ankits@gmail.com>
2023-12-21 15:13:33 +0530

    Update alien-dictionary.py

--- Alien Dictionary - GFG/alien-dictionary.py
+++ Alien Dictionary - GFG/alien-dictionary.py
@@ -1,6 +1,8 @@
 ### Answer 1 - Using Kahn's algorithm 
 ### Time complexity - O(N*M + K + K + Q) ~ O(N*M), Space complexity - O(K + N + N)~O(N)
 ### Here M is the length of the largest word in the array, Q is the queue
+### Intuition
+### to check all the consecutive pairs of the words and find the differentiating factor and map them as a graph, to get the problem down to topological sort.
 from collections import defaultdict,deque
 class Solution:
     def findOrder(self,alien_dict, N, K):
