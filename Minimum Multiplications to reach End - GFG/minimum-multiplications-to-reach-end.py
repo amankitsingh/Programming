ee26088704b9585a20094ac9c558b1602190552c
Ankit Singh <1999ankits@gmail.com>
2023-12-22 15:31:56 +0530

    Update minimum-multiplications-to-reach-end.py

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.py
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.py
@@ -1,4 +1,5 @@
-#User function Template for python3
+### Answer 1 - using BFS and graph traversal
+### Time complexity - O(mod*N), Space complexity - O(mod*N)
 from typing import List
 from collections import deque
 class Solution:
@@ -24,17 +25,3 @@ class Solution:
                     queue.append((multiple, steps+1))
     
         return -1
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__=="__main__":
-    for _ in range(int(input())):
-        n = int(input())
-        arr = [int(x) for x in input().strip().split()]
-        start, end = list(map(int,input().split()))
-        obj=Solution()
-        print(obj.minimumMultiplications(arr, start, end))
-# } Driver Code Ends
