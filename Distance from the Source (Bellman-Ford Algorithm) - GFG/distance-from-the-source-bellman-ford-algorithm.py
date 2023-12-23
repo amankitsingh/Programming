9104e54088113ef4f91c156e1cf307d55257c7f3
Ankit Singh <1999ankits@gmail.com>
2023-12-23 15:00:35 +0530

    Update distance-from-the-source-bellman-ford-algorithm.py

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.py
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.py
@@ -1,4 +1,5 @@
-#User function Template for python3
+### Bellman Ford algorithm
+### Time complexity - O(V*E + E) ~ O(V*E), Space complexity - O(V)
 from collections import defaultdict,deque
 class Solution:
     '''
@@ -28,29 +29,3 @@ class Solution:
                 return [-1]
     
         return distance
-            
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import atexit
-import io
-import sys
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        V,E = map(int,input().strip().split())
-        edges = []
-        for i in range(E):
-            u,v,w = map(int,input().strip().split())
-            edges.append([u,v,w])
-        S=int(input())
-        ob = Solution()
-        
-        res = ob.bellman_ford(V,edges,S)
-        for i in res:
-            print(i,end=" ")
-        print()
-# } Driver Code Ends
