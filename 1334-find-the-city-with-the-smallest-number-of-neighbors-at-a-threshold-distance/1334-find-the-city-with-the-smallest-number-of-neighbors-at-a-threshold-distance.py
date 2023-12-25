f277984fb6f2af0dbe641e4301f5d06edf22494c
Ankit Singh <1999ankits@gmail.com>
2023-12-25 18:08:43 +0530

    Time: 484 ms (58.26%), Space: 18.5 MB (7.36%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,32 @@
+import heapq
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        inf = 10000000000
+        adjmatrix = [[inf]*n for _ in range(n)]
+
+        for node_from,node_to,weight in edges:
+            adjmatrix[node_from][node_to] = weight
+            adjmatrix[node_to][node_from] = weight
+        
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if i==j:
+                        adjmatrix[i][j]=0
+                        continue
+                    mini = min(adjmatrix[i][j], adjmatrix[i][k]+adjmatrix[k][j])
+                    adjmatrix[i][j] = mini
+                    
+        cityNumber,countCity = 0,n
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if adjmatrix[i][j] > distanceThreshold or adjmatrix[i][j] == inf:
+                    adjmatrix[i][j] = 0
+                if adjmatrix[i][j] > 0:
+                    count+=1
+            if count<=countCity:
+                countCity = count
+                cityNumber = i
+                
+        return cityNumber
