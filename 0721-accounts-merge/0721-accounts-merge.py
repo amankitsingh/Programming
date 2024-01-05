08c73806f46f7aeb1417735e85c9f3b3fe345560
Ankit Singh <1999ankits@gmail.com>
2024-01-05 16:10:48 +0530

    Time: 175 ms (94.50%), Space: 21 MB (65.45%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,34 @@
+class DisjointSet:
+    def __init__(self, size):
+        self.parents = list(range(size))
+        
+    def findparent(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.findparent(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, u, v):
+        self.parents[self.findparent(u)] = self.findparent(v)
+        
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        emailhashmap = {}
+        n = len(accounts)
+        dsu = DisjointSet(n)
+        
+        sorted(accounts)
+        
+        for index, emails in enumerate(accounts):
+            for i in range(1,len(emails)):
+                if emails[i] not in emailhashmap:
+                    emailhashmap[emails[i]] = index
+                else:
+                    dsu.union(index, emailhashmap[emails[i]])
+                    
+        
+        ans = collections.defaultdict(list)
+        for email, owner in emailhashmap.items():
+            ans[dsu.findparent(owner)].append(email)
+            
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
+    
