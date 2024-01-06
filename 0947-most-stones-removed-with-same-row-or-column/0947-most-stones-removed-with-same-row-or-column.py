c3a6d685c853515e9a296c11f66a96afa7984386
Ankit Singh <1999ankits@gmail.com>
2024-01-06 17:47:51 +0530

    Update 0947-most-stones-removed-with-same-row-or-column.py

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,4 +1,49 @@
 ### Answer 1 - Using Disjoint Set
+### Time complexity - O(N+N*E*4*aplha)~O(N), Space complexity - O(N*2 + N)~O(N)
+class DisjointSet:
+    def __init__(self, size):
+        self.size = [1]*size
+        self.parent = list(range(size))
+    
+    def findparent(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.findparent(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, u, v):
+        ulp_u,ulp_v = self.findparent(u), self.findparent(v)
+        if ulp_v == ulp_u:
+            return
+        if self.size[ulp_u] < self.size[ulp_v]:
+            ulp_u, ulp_v = ulp_v, ulp_u
+        self.parent[ulp_v] = self.parent[ulp_u]
+        self.size[ulp_u]+=self.size[ulp_v]
+        
+class Solution:
+    def maxRemove(self, stones, n):
+        maxRow = 1
+        maxCol = 1
+        #n = len(stones)
+        for i,j in stones:
+            maxRow = max(maxRow, i)
+            maxCol = max(maxCol, j)
+        
+        dsu = DisjointSet(maxRow+maxCol+2)
+        stoneNode = {}
+        for i,j in stones:
+            nodeRow = i
+            nodeCol = j+maxRow+1
+            dsu.union(nodeRow, nodeCol)
+            stoneNode[nodeRow] = 1
+            stoneNode[nodeCol] = 1
+        cnt = 0
+        for i in stoneNode.keys():
+            if dsu.findparent(i) == i:
+                cnt+=1
+        return n-cnt
+
+
+### Answer 2 - Using Disjoint Set
 ### Time complexity - O(N+E*4*aplha)~O(N), Space complexity - O(N*2 + N + N)~O(N)
 class DisjointSet:
     def __init__(self, size):
