8ca0c444b5ae8cf3058fe5870d6acd16675638b7
Ankit Singh <1999ankits@gmail.com>
2024-01-06 17:34:45 +0530

    Time: 118 ms (99.39%), Space: 18 MB (46.55%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,39 @@
+class DisjointSet:
+    def __init__(self, size):
+        self.size = [1]*size
+        self.parent = list(range(size))
+    
+    def findparent(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.findparent(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, u, v):
+        ulp_u,ulp_v = self.findparent(u), self.findparent(v)
+        if ulp_v == ulp_u:
+            return 0
+        if self.size[ulp_u] < self.size[ulp_v]:
+            ulp_u, ulp_v = ulp_v, ulp_u
+        self.size[ulp_u]+=self.size[ulp_v]
+        self.parent[ulp_v] = self.parent[ulp_u]
+        return 1
+    
+            
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        dsu = DisjointSet(n)
+        rows, cols = {}, {}
+        removed = 0
+        for i, (row, col) in enumerate(stones):
+            if row in rows:
+                removed += dsu.union(i, rows[row])
+            else:
+                rows[row] = i
+            if col in cols:
+                removed += dsu.union(i, cols[col])
+            else:
+                cols[col] = i
+        
+        return removed
+                
