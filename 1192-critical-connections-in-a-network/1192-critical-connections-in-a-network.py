444f9c872f125c31ff3d7a637b6f00857039e4d9
Ankit Singh <1999ankits@gmail.com>
2024-01-07 19:51:39 +0530

    Time: 1607 ms (94.53%), Space: 67.9 MB (96.98%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+class Solution:
+    def dfs(self, node, parent):
+        self.visited[node] = 1
+        self.time[node] = self.low[node] = self.timer
+        self.timer+=1
+        for node_to in self.adj[node]:
+            if node_to == parent:
+                continue
+            if self.visited[node_to] == 0:
+                self.dfs(node_to, node)
+                # take the lowest of all the adjacent nodes
+                self.low[node] = min(self.low[node_to], self.low[node])
+                #check if a bridge can be made
+                if self.low[node_to] > self.time[node]:
+                    self.bridges.append([node,node_to])
+            else:
+                # you have visited it, no bridge possible, just take the minium lowest time of insertion
+                self.low[node] = min(self.low[node], self.low[node_to])
+                
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        self.adj = defaultdict(list)
+        for node_from, node_to in connections:
+            self.adj[node_from].append(node_to)
+            self.adj[node_to].append(node_from)
+        
+        self.timer = 1
+        self.visited = [0]*n
+        self.time = [None]*n
+        self.low = [None]*n
+        self.bridges = []
+        self.dfs(0,-1)
+        return self.bridges
