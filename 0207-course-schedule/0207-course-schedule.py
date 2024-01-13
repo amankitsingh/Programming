d695b6c9ea4e7ccb3e43c07d5fc5b67d9548c9a3
Ankit Singh <1999ankits@gmail.com>
2024-01-13 15:33:37 +0530

    Update 0207-course-schedule.py

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,5 +1,5 @@
 ### Answer 1 - DFS
-### Time complexity - O(N*N), Space complexity - O(N)+O(N)+O(N)
+### Time complexity - O(N*N), Space complexity - O(N)+O(N)+O(N)~O(N)
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = defaultdict(list)
@@ -24,3 +24,32 @@ class Solution:
                 return False
         else:
             return True
+
+### Answer 2 - Using Kahn's algorithm - BFS
+### Time complexity - O(V+E), Space complexity - O(N)+O(N)+O(N) ~O(N)
+### Better solution
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        queue = deque()
+        indegree = [0]*(numCourses+1)
+        result = []
+
+        for cou_from, cou_to in prerequisites:
+            adj[cou_from].append(cou_to)
+            indegree[cou_to]+=1
+
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        while queue:
+            node = queue.popleft()
+            result.append(node)
+            for it in adj[node]:
+                indegree[it]-=1
+                if indegree[it] == 0:
+                    queue.append(it)
+
+        return len(result) == numCourses
