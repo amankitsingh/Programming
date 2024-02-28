68905942a06ea132cdaafe14e551c118097b52b2
Ankit Singh <1999ankits@gmail.com>
2024-02-29 00:00:55 +0530

    Time: 996 ms (29.48%), Space: 17.3 MB (57.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,23 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
-        dp = [[-1]*(amount+1) for _ in range(n)]
-        def coin(index, target):
-            if index == 0:
-                if target%coins[0] == 0:
-                    return target//coins[0]
-                else:
-                    return int(1e9)
-            if dp[index][target]!=-1:
-                return dp[index][target]
-            nonpick = coin(index-1, target)
-            pick = int(1e9)
-            if coins[index]<=target:
-                pick = 1+coin(index,target-coins[index])
-            dp[index][target] = min(nonpick, pick)
-            return dp[index][target]
-        ans = coin(n-1, amount)
-        return -1 if ans == int(1e9) else ans
+    def coinChange(self, arr: List[int], T: int) -> int:
+        n = len(arr)
+        prev = [0] * (T + 1)
+        cur = [0] * (T + 1)
+        
+        for i in range(0, 1 + T):
+            if i % arr[0] == 0:
+                prev[i] = i // arr[0]
+            else:
+                prev[i] = int(1e9)
+        for ind in range(1, n):
+            for target in range(T + 1):
+                not_take = prev[target]
+                take = int(1e9)
+                if arr[ind] <= target:
+                    take = 1 + cur[target - arr[ind]]
+                cur[target] = min(not_take, take)
+            prev = cur 
+        ans = prev[T]
+        if ans >= int(1e9):
+            return -1
+        return ans
