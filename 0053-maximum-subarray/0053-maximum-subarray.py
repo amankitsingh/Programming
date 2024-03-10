5e6dc625082850f49f8dd19466ca456ed38ed8e9
Ankit Singh <1999ankits@gmail.com>
2024-03-10 23:42:35 +0530

    Update 0053-maximum-subarray.py

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,3 +1,19 @@
+### Answer 1 - using 2 pointer
+### Time complexity - O(N), space complexity - o(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = nums[0]
+        currentsum = 0
+        for num in nums:
+            if currentsum < 0:
+                currentsum = 0
+            currentsum+=num
+            maxsum = max(maxsum,currentsum)
+            
+        return maxsum
+
+### Answer 2 - using dp
+### Time complexity - O(N), space complexity - O(N)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
