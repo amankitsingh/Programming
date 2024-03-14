130872cd9700569014627de5ae9f4d8ee7b6f2a5
Ankit Singh <1999ankits@gmail.com>
2024-03-14 18:43:14 +0530

    Update move-zeroes.py

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -1,15 +1,14 @@
+### Slidin window
+### Time complexity - O(N), space complexity-O(1)
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         if len(nums)<2:
             return
-        i,j= 0,1
+        i,j=0,1
         while j < len(nums):
-            if nums[i] == 0 and nums[j] != 0:
+            if nums[i]==0 and nums[j]!=0:
                 nums[i],nums[j]=nums[j],nums[i]
                 i+=1
-                j+=1
-            elif nums[i] != 0 and nums[j] == 0:
-                i = j
-                j+=1
-            else:
-                j+=1
+            elif nums[i]!=0 and nums[j]==0:
+                i=j
+            j+=1
