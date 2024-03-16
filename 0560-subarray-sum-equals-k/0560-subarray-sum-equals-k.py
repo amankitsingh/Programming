3901ea04f00d25443e5e7d76f452f4304b1599c3
Ankit Singh <1999ankits@gmail.com>
2024-03-16 13:13:32 +0530

    Time: 242 ms (21.24%), Space: 20.7 MB (11.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,6 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        result_map = collections.Counter({0:1})
+        result_map = defaultdict(int)
+        result_map[0]=1
         n = len(nums)
         prevSum = 0
         cnt = 0
