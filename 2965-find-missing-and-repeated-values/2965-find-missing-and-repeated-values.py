de54a3e4a8f45fac0ec4519314453eec1eb6dba7
Ankit Singh <1999ankits@gmail.com>
2024-03-19 23:05:18 +0530

    Update 2965-find-missing-and-repeated-values.py

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -1,3 +1,24 @@
+### Answer 1 - Brute force
+### TC - O(N^2), SC - O(1)
+def findMissingRepeatingNumbers(a: [int]) -> [int]:
+    rep,mis= -1,-1
+    n= len(a)
+    for i in range(1,n+1):
+        cnt=0
+        for j in range(n):
+            if a[j] == i:
+                cnt+=1
+        if cnt==2:
+            rep = i
+        elif cnt == 0:
+            mis = i
+        
+        if rep!=-1 and mis!=-1:
+            break
+    return [rep,mis]
+
+### Answer 2 - Using hash map
+### TC - O(n*n + n + n)~O(n), SC - O(n)
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         hasp = defaultdict(int)
@@ -19,3 +40,4 @@ class Solution:
             if hasp[i]>1:
                 rep = i
         return [rep,mis]
+
