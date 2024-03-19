1f64e3164639c6d6718c79661b9186a37dd47ee0
Ankit Singh <1999ankits@gmail.com>
2024-03-19 15:59:59 +0530

    Update 4sum-ii.py

--- 4sum-ii/4sum-ii.py
+++ 4sum-ii/4sum-ii.py
@@ -1,3 +1,5 @@
+### Answer 1 - optimal approach
+### TC - O(N^2), SC- O(N)
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         dic = collections.defaultdict(int)
