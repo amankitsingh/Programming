23358da192e7fb2373b090bcc0062d376b13ec15
Ankit Singh <1999ankits@gmail.com>
2024-03-26 19:08:24 +0530

    Update 0152-maximum-product-subarray.py

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,3 +1,33 @@
+### Answer 1 - Brute force
+### TC - O(n^3), SC-O(1)
+class Solution:
+    def maxProduct(self, arr: List[int]) -> int:
+        result = float('-inf')
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                prod = 1
+                for k in range(i, j + 1):
+                    prod *= nums[k]
+                result = max(result, prod)
+        return result
+
+### Answer 2 - Better than brute force
+### TC - O(n^2), SC-O(1)
+class Solution:
+    def maxProduct(self, arr: List[int]) -> int:
+        n = len(arr)
+        ans = arr[0]
+        for i in range(n):
+            prod = arr[i]
+            for j in range(i+1,n):
+                ans = max(ans, prod)
+                prod*=arr[j]
+            ans = max(ans, prod)
+        return ans
+
+### Answer 3 - Optimal solution
+### TC - O(n), SC-O(1)
+### intuition - if 0 then the product becomes 0, if odd -ve number then ans is -ve, if even -ve then ans is +ve
 class Solution:
     def maxProduct(self, arr: List[int]) -> int:
         n = len(arr)
