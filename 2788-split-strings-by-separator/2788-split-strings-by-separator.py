723eeefbcf2bc00510c91387b24a57ae93cea981
Ankit Singh <1999ankits@gmail.com>
2024-04-07 14:12:43 +0530

    Update 2788-split-strings-by-separator.py

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.py
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.py
@@ -5,3 +5,12 @@ class Solution:
             temp = [x.strip() for x in word.split(separator) if x]
             result.extend(temp)
         return result
+
+
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        result = ""
+        for word in words:
+            result+=word
+            result+=separator
+        return result.replace(separator,"").split()
