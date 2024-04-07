617ee383bb4567c91222ed8f207bb63286ac4269
Ankit Singh <1999ankits@gmail.com>
2024-04-07 14:05:27 +0530

    Update 1221-split-a-string-in-balanced-strings.py

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -1,3 +1,17 @@
+### Answer 1 - keeping hash
+### TC-O(n), SC - O(1)
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        sump = {"L":0,"R":0}
+        result = 0
+        for word in s:
+            sump[word]+=1
+            if sump["L"] == sump["R"]:
+                result+=1
+        return result
+
+### Answer 2 - using 2 variable
+### TC - O(n), SC - O(1)
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
         balance = 0
