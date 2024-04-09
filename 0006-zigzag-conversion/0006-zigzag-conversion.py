07aaf9cc5f4181fdda033d50315f35f2f8c00d0d
Ankit Singh <1999ankits@gmail.com>
2024-04-10 00:22:02 +0530

    Update 0006-zigzag-conversion.py

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -25,3 +25,29 @@ class Solution:
         for i in range(numRows):
             result+="".join(matrix[i]).strip()
         return result
+
+### Answer 2
+### TC - O(N), SC(R)
+### N-len of string, R-no of rows.
+### Intution
+'''
+take 2 pointer and add the value of array, if i become 0 that means we are adding it to the next element,if i become n-1 that mean we are adding to prev element.
+'''
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1:
+            return s
+        template = [""] * numRows
+
+        i = 0
+        j = 1
+        for element in s:
+            template[i]+= element
+            if i == 0:
+                j = 1
+            elif i == numRows -1:
+                j = -1
+            i += j
+
+        result = "".join(template)
+        return result
