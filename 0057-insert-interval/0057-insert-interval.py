40ee4d973762e96c973e30251e69eb1941b95fdb
Ankit Singh <1999ankits@gmail.com>
2024-04-13 23:03:33 +0530

    Update 0057-insert-interval.py

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -17,3 +17,38 @@ class Solution:
             else:
                 result[-1][1] = max(interval[1], result[-1][1])
         return result
+
+
+### Answer 2 - after observation
+### TC - O(N), SC - O(1)
+'''
+Intution
+we observed that there are 3 case
+1. when there is no overlap
+2. when there is overlap, at that we need to take min of interval and new interval for start and max for end
+3. when there is no overlap
+'''
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i = 0
+        result = []
+        
+        #case 1: when no overlap
+        while i<n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i+=1
+        
+        #case 2: when overlap
+        while i<n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i+=1
+        result.append(newInterval)
+    
+        
+        #case 3: when overlaps are merged
+        while i<n:
+            result.append(intervals[i])
+            i+=1
+        return result
