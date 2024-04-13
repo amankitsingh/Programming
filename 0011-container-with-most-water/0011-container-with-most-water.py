afc55aac92ff635c28d593ad5e2346c5e69a5a08
Ankit Singh <1999ankits@gmail.com>
2024-04-13 17:47:35 +0530

    Update 0011-container-with-most-water.py

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,3 +1,25 @@
+### Answer 1
+'''
+Intuition
+we take 2 pointers, left and right
+and take the total with the container
+find which is lowest and multiple with the distance between the left and right and lowest width,
+'''
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n=len(height)
+        left,right,max_width = 0, n-1,n-1
+        area = 0
+        for width in range(max_width, 0,-1):
+            if height[left]<height[right]:
+                area = max(area, height[left]*width)
+                left+=1
+            else:
+                area = max(area, height[right]*width)
+                right-=1
+        return area
+
+### Answer 2
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
