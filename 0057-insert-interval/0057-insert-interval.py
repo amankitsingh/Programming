9236f541883173eaca777b33eb3eb6a94d27626e
Ankit Singh <1999ankits@gmail.com>
2024-04-13 22:55:10 +0530

    Update 0057-insert-interval.py

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,3 +1,10 @@
+### Answer - similar to https://github.com/amankitsingh/Programming/tree/master/0056-merge-intervals
+### TC - O(NlogN+N), SC - O(N)
+'''
+Intution:
+append the new interval to the intervals list and sort the array based on the first element
+then check if the last element is greater or less the the result element and expand the array list accordingly 
+'''
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         intervals.append(newInterval)
