c4eab0ae354cc84db0bcec9fab441c3c37b11526
Ankit Singh <1999ankits@gmail.com>
2024-04-14 15:34:43 +0530

    Update 0074-search-a-2d-matrix.py

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,5 +1,9 @@
 ### Answer 1 - Using binary search
 ### TC - O(N*logM), SC - O(1)
+'''
+Intuition:
+if the matrix is sorted then we can do binary search on each row and find the target
+'''
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         n = len(matrix)
@@ -21,3 +25,19 @@ class Solution:
             if matrix[i][0] <= target and target <= matrix[i][m-1]:
                 return search_using_binary_search(matrix[i],target)
         return False
+
+### Answer 2 - Using recursion
+### TC - O(N+M), SC - O(1)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def search(i,j):
+            if 0<=i<len(matrix) and 0<=j<len(matrix[0]):
+                if matrix[i][j] == target:
+                    return True
+                elif matrix[i][j] > target:
+                    return search(i-1,j)
+                else:
+                    return search(i,j+1)
+            else:
+                return False
+        return search(len(matrix)-1,0)
